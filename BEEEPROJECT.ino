d8807aa958aac9a49b89ccd8f6499c4f0d223b57
hrithiksaxena854 <45188840+hrithiksaxena854@users.noreply.github.com>
2018-11-20 08:36:10 +0530

    Add files via upload

---
+++ BEEEPROJECT.ino
@@ -0,0 +1,17 @@
+int ledPin=10;
+int pirPin=9;
+int val=0;
+void setup() 
+{
+ pinMode(ledPin,OUTPUT);
+ pinMode(pirPin,INPUT);
+}
+void loop()
+{
+   val=digitalRead(pirPin);
+   digitalWrite(ledPin,val);
+   if(val==1)
+   {
+    digitalWrite(ledPin,LOW);
+   }
+}
